98c77b4757683855721ce996eea0d578684abac1
SashaHon <oleksandra.honchar@powercoders.org>
2023-11-14 11:10:47 +0100

    initialize project

---
+++ movies-mocks.js
@@ -0,0 +1,19 @@
+export const moviesList = [
+  // {}, here we can store our movies objects
+];
+
+//I believe we might need this variable too, to store the connection of
+//the dislayed emoji and the emotion that is connected to it. OR we can
+//make this connection through the "Switch" function etc, so it's up to you
+//to decide the best solution <3
+//just wanted to leave it here. feel free to delete the comments
+export const emotions = [
+  //   {
+  //     code: emoji,
+  //     emotion: emotionName,
+  //   },
+  //   {
+  //     code: emoji,
+  //     emotion: emotionName,
+  //   },
+];
---
+++ script.js
@@ -0,0 +1,4 @@
+import { moviesList, emotions } from "./movies-mocks.js";
+
+//your code here, you can now address variables moviesList and emotions
+//from the import above;
