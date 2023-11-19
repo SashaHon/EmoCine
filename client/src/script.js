157fd4e50f7adc26f87905222c085934f5e495c4
SashaHon <oleksandra.honchar@powercoders.org>
2023-11-19 16:01:27 +0100

    create handleSubmit event on form which sends to server 'emotion' and gets back the list of movies from db related to that emotion

--- client/src/script.js
+++ client/src/script.js
@@ -1,16 +1,44 @@
-const title = document.querySelector("#title");
-const description = document.querySelector("#description");
+const form = document.querySelector("#form");
+form.addEventListener("submit", handleSubmit);
 
-async function getMoviesData(link) {
-  const response = await fetch(link);
-  const movieData = await response.json();
+async function handleSubmit(e) {
+  e.preventDefault();
 
-  // console.log(movieData);
-  movieData.map((obj) => {
-    title.textContent = obj.Title;
-    description.textContent = `Year: ${obj.Year} 
-      Genre: ${obj.Genre}
-      IMBD Rating: ${obj.Rating}`;
-  });
+  const formData = new FormData(form);
+
+  try {
+    const response = await fetch("http://localhost:3005/movies-db", {
+      method: "POST",
+      body: parseFormData(formData),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+    const moviesData = await response.json();
+    // console.log(moviesData);
+    showMovieSuggestion(moviesData); // main function where we assign movies data to the page elements;
+  } catch (error) {
+    console.error("Error:", error);
+  }
 }
-getMoviesData("http://localhost:3005/movies");
+
+function parseFormData(formData) {
+  let obj = {};
+  for (let [key, value] of formData.entries()) {
+    obj[key] = value;
+  }
+
+  return JSON.stringify(obj);
+}
+
+function showMovieSuggestion(data) {
+  const title = document.querySelector("#title");
+  const description = document.querySelector("#description");
+
+  title.textContent = data[0].title; // assign movie's title to the title in the first object of the array;
+  //need more code and logic here;
+}
--- server/index.js
+++ server/index.js
@@ -6,33 +6,23 @@ const app = express();
 const port = 3005;
 const connection = mysql.createConnection(process.env.DATABASE_URL);
 
-// connection.connect((err) => {
-//   if (err) {
-//     console.error("Error connecting to PlanetScale:", err.stack);
-//     return;
-//   }
-//   console.log("Connected to PlanetScale!");
-
-//   // Perform operations on the database here, such as executing queries
-//   // For example:
-//   connection.query("SELECT * FROM Movies", (error, results) => {
-//     if (error) throw error;
-//     console.log("Fetched data:", results);
-//   });
-
-//   connection.end(); // Close the connection when done with database operations
-// });
-
 app.use(cors());
+app.use(express.urlencoded({ extended: true })); // Parse URL-encoded bodies
+app.use(express.json()); // Parse JSON bodies
 
-app.get("/movies", function (request, respond) {
-  connection.query("SELECT * FROM Movies", (error, results) => {
-    if (error) {
-      respond.status(500).json({ error: "Error fetching data" });
-    } else {
-      respond.json(results);
+app.post("/movies-db", (request, respond) => {
+  const emotion = request.body.emotion;
+
+  connection.query(
+    `SELECT * FROM Movies WHERE emotion='${emotion}'`,
+    (error, results) => {
+      if (error) {
+        respond.status(500).json({ error: `Error fetching data, ${error}` });
+      } else {
+        respond.json(results);
+      }
     }
-  });
+  );
 });
 
 app.listen(port, () => {
