bfc1a2eb7bfb5bf8fb96b09b0843af7427e43034
faranak <faranak.rahimi@powercoders.org>
2023-11-21 11:10:40 +0100

    add css style

--- client/src/script.js
+++ client/src/script.js
@@ -32,29 +32,26 @@ function showMovieSuggestion(data) {
   if (list.childElementCount !== 0) {
     list.innerHTML = "";
   }
-
   data.map((obj) => {
     const listItem = document.createElement("li");
     const embedLink = obj.trailerLink.replace(/watch\?v=/, "embed/");
     console.log("here", embedLink);
-
     listItem.innerHTML = `
     <article>
       <h2 id="title">${obj.title}</h2>
+      <div id="wrapper">
+      <img id="poster" src=${obj.imgUrl} alt="${obj.title} poster">
+       <iframe id="trailer" width="420" height="345" src=${embedLink} title="video"></iframe>
+     </div>
       <ul id="description">
         <li id="year">Year: ${obj.year}</li>
         <li id="genre">Genre: ${obj.genre}</li>
         <li id="rating">IMDB Rating: ${obj.imdbRating}</li>
         <li id="imdblink"><a href=${obj.imdbLink} target='_blank'>See more info at IMDB</a></li>
       </ul>
-      <div id="wrapper">
-       <img src=${obj.imgUrl} alt="${obj.title} poster">
-        <iframe id="trailer" width="420" height="345" src=${embedLink} title="video"></iframe>
-      </div>
+     
     </article >
     `;
-
     list.append(listItem);
   });
 }
-
